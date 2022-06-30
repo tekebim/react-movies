242069c71374ac37a7dcaed1795eceeb53ac0127
Michael R <tekmedias@gmail.com>
2022-06-30 11:55:13 +0200

    :bug: Fix add form error

--- src/components/AddMovies.js
+++ src/components/AddMovies.js
@@ -2,7 +2,6 @@ import {gql, useMutation} from "@apollo/client";
 import {useState} from "react";
 
 const AddMovies = () => {
-  const [showAddMovieForm, setShowAddMovieForm] = useState(false);
   const [title, setTitle] = useState("");
   const [plot, setPlot] = useState("");
   const [poster, setPoster] = useState("");
@@ -22,52 +21,31 @@ const AddMovies = () => {
   if (loading) return <p>Chargement...</p>;
   if (error) return `Error! ${error.message}`;
 
-  const AddFormMovie = () => {
-    return <div className="form-add-movie">
-      <div className="form-input">
-        <label htmlFor="title">Titre du film</label>
-        <input type="text" id="title" onChange={(e) => {
-          setTitle(e.target.value);
-        }}/>
-      </div>
-      <div className="form-input">
-        <label htmlFor="plot">Synopsis du film</label>
-        <input type="text" id="plot" onChange={(e) => {
-          setPlot(e.target.value);
-        }}/>
-      </div>
-      <div className="form-input">
-        <label htmlFor="poster">Poster URL </label>
-        <input type="text" id="poster" onChange={(e) => {
-          setPoster(e.target.value);
-        }}/>
-      </div>
-      <div className="form-input">
-        <input type="submit" value={"Valider"}
-               onClick={() => addMovie({variables: {movie: {title: title, plot: plot}}})}/>
-        <button onClick={() => handleCTAClick()}>Close</button>
-      </div>
+  return <div className="form-add-movie">
+    <div className="form-input">
+      <label htmlFor="title">Titre du film</label>
+      <input type="text" id="title" onChange={(e) => {
+        setTitle(e.target.value);
+      }}/>
     </div>
-  }
-
-  const handleCTAClick = () => {
-    setShowAddMovieForm(!showAddMovieForm);
-  }
-
-  const AddCTAMovie = () => {
-    return <div className={"add-movie-wrapper"}>
-      <button onClick={() => handleCTAClick()}>Add a movie</button>
+    <div className="form-input">
+      <label htmlFor="plot">Synopsis du film</label>
+      <input type="text" id="plot" onChange={(e) => {
+        setPlot(e.target.value);
+      }}/>
     </div>
-  }
+    <div className="form-input">
+      <label htmlFor="poster">Poster URL </label>
+      <input type="text" id="poster" onChange={(e) => {
+        setPoster(e.target.value);
+      }}/>
+    </div>
+    <div className="form-input">
+      <input type="submit" value={"Valider"}
+             onClick={() => addMovie({variables: {movie: {title: title, plot: plot}}})}/>
+    </div>
+  </div>
 
-  return (
-    <>
-      {!showAddMovieForm ?
-        <AddCTAMovie/> :
-        <AddFormMovie/>
-      }
-    </>
-  );
 }
 
 export default AddMovies;
