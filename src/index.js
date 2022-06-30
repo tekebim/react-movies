87d04776a189c8b86520720d363ad2c75b0334f1
Michael R <tekmedias@gmail.com>
2022-06-30 10:08:26 +0200

    :sparkles: Add apolloProvider

--- src/index.js
+++ src/index.js
@@ -12,23 +12,26 @@ const client = new ApolloClient({
 
 // L'instance d'ApolloClient permet d'établir une connexion avec le serveur. L'instance va aussi nous proposer des méthodes pour travailler efficacement avec graphQL
 
-client.query({
-  query: gql`
-    query Movies {
-        movies {
-            title,
-            plot
-        }
-    }
-  `
-}).then((result) => console.log(result));
+// client.query({
+//   query: gql`
+//     query Movies {
+//         movies {
+//             title,
+//             plot
+//         }
+//     }
+//   `
+// }).then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
   </React.StrictMode>
 );
+// ApolloProvider permet d'intégrer le client sur des composants spécifiques (ici, cela intègre l'intégralité de notre application).
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
